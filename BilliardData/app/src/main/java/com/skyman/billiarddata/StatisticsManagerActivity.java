677f76c57f38834991c8066a8dfed0fa9c575752
Jang Dongik <69628757+skymanlab@users.noreply.github.com>
2023-05-12 16:27:22 +0900

    폐기 완료
    폐기 내용 저장 후 파일 삭제

--- BilliardData/app/src/main/java/com/skyman/billiarddata/StatisticsManagerActivity.java
+++ BilliardData/app/src/main/java/com/skyman/billiarddata/StatisticsManagerActivity.java
@@ -1,7 +1,6 @@
 package com.skyman.billiarddata;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +8,12 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
-import com.skyman.billiarddata.developer.DeveloperManager;
 import com.skyman.billiarddata.developer.Display;
 import com.skyman.billiarddata.etc.SectionManager;
 import com.skyman.billiarddata.etc.SessionManager;
 import com.skyman.billiarddata.etc.calendar.SameDate;
 import com.skyman.billiarddata.etc.calendar.SameDateCheckerUtil;
 import com.skyman.billiarddata.etc.database.AppDbManager;
-import com.skyman.billiarddata.etc.statistics.Date;
-import com.skyman.billiarddata.etc.statistics.Record;
-import com.skyman.billiarddata.etc.statistics.SameDateChecker2;
-import com.skyman.billiarddata.etc.statistics.StatisticsAnalysis;
 import com.skyman.billiarddata.fragment.statistics.StatisticsViewPagerAdapter;
 import com.skyman.billiarddata.table.billiard.data.BilliardData;
 import com.skyman.billiarddata.table.billiard.database.BilliardDbManager2;
@@ -27,12 +21,7 @@ import com.skyman.billiarddata.table.player.data.PlayerData;
 import com.skyman.billiarddata.table.player.database.PlayerDbManager2;
 import com.skyman.billiarddata.table.user.data.UserData;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class StatisticsManagerActivity extends AppCompatActivity implements SectionManager.Initializable {
 
@@ -124,11 +113,6 @@ public class StatisticsManagerActivity extends AppCompatActivity implements Sect
 
                 SameDateCheckerUtil sameDateCheckerUtil = new SameDateCheckerUtil();
                 sameDate = sameDateCheckerUtil.createSameDate(userData, billiardDataArrayList);
-
-                SameDateChecker2 sameDateChecker2 = new SameDateChecker2();
-                StatisticsAnalysis analysis = new StatisticsAnalysis(appDbManager);
-                analysis.analyzeMonthTotalStats(sameDateChecker2.checkSameDate(userData, billiardDataArrayList), billiardDataArrayList);
-
 
             }
 
